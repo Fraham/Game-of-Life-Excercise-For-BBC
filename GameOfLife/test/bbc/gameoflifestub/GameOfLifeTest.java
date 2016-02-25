55a175e205df4d1912ddfd3b85424537f2dae400
Fraham <frahamp@gmail.com>
2016-02-25 02:05:19 +0000

    Setup for the test for game of life

--- GameOfLife/test/bbc/gameoflifestub/GameOfLifeTest.java
+++ GameOfLife/test/bbc/gameoflifestub/GameOfLifeTest.java
@@ -1,13 +1,34 @@
 package bbc.gameoflifestub;
 
+import java.util.HashSet;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class GameOfLifeTest {
 	
+	private GameOfLife testGameOfLife;
+	HashSet<Cell> currentSetOfCells;
+	HashSet<Cell> nextSetOfCells;
+	
 	@Before
 	public void setUp(){
-    	
+		testGameOfLife = new GameOfLife();
+		testGameOfLife.setHeight(100);
+		testGameOfLife.setWidth(100);
+		
+		currentSetOfCells = new HashSet<Cell>();
+		currentSetOfCells.add(new Cell(1,1));
+		currentSetOfCells.add(new Cell(2,2));
+        Life currentLife = new Life(currentSetOfCells);
+        
+        nextSetOfCells = new HashSet<Cell>();
+        nextSetOfCells.add(new Cell(2,1));
+        nextSetOfCells.add(new Cell(1,2));
+        Life nextLife = new Life(nextSetOfCells);
+        
+        testGameOfLife.setCurrentLife(currentLife);
+        testGameOfLife.setNextLife(nextLife);
     }
 	
 	@Test
